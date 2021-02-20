8b1b22a280632225eced8a349b0c142564a67d66
Samuel Carnell <samuel.carnell@outlook.com>
2021-02-20 13:42:56 +0000

    Comment useObservableState

--- src/use-observable-state.ts
+++ src/use-observable-state.ts
@@ -2,15 +2,21 @@ import { useState } from 'react';
 import { Observable } from 'rxjs';
 import { useSubscription } from './helpers/use-subscription';
 
-type ObservableState<TValue, TError> = {
-	value: TValue | undefined;
-	error: TError | undefined;
-	isComplete: boolean;
-} & [TValue | undefined, TError | undefined, boolean];
-
+/**
+ * Stores and returns the current state of the given observable `observable`.
+ * This state will get updated whenever the observable emits a new value, throws an error or completes,
+ * and will trigger a re-render of the component.
+ * @typeParam TValue The type of the value emitted by the observable `observable`.
+ * @typeParam TError The type of error throw by the observable `observable`. Defaults to `any`.
+ * @param observable The observable to store and return the state of.
+ * @returns An array with each value in the array being
+ *  1. The latest value emitted by the observable. If the observable has not yet emitted a value this will be undefined.
+ *  2. The error thrown by the observable. If the observable has not yet thrown a value this will be undefined.
+ *  3. A boolean representing if the observable is complete.
+ */
 export function useObservableState<TValue, TError = any>(
 	observable: Observable<TValue>
-): ObservableState<TValue, TError> {
+): [TValue | undefined, TError | undefined, boolean] {
 	const [value, setValue] = useState<TValue | undefined>(undefined);
 	const [error, setError] = useState<TError | undefined>(undefined);
 	const [isComplete, setIsComplete] = useState<boolean>(false);
@@ -29,10 +35,5 @@ export function useObservableState<TValue, TError = any>(
 		});
 	}, [observable]);
 
-	const observableState: any = [value, error, isComplete];
-	observableState.value = value;
-	observableState.error = error;
-	observableState.isComplete = isComplete;
-
-	return observableState as ObservableState<TValue, TError>;
+	return [value, error, isComplete];
 }

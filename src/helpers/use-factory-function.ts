f3ca277d51e0056e89a3bd49cc2afaed43086b9a
Samuel Carnell <samuel.carnell@outlook.com>
2021-02-21 14:20:33 +0000

    Rename variables for useFactoryFunction

--- src/helpers/use-factory-function.ts
+++ src/helpers/use-factory-function.ts
@@ -5,20 +5,20 @@ import { useHasInputChanged } from './use-has-input-changed';
  * Uses the provided `factoryFunction` to compute the returned value. This value persists across renders, only being recomputed if any of the dependencies change.
  * @typeParam TValue The type of value produced by the `factoryFunction`.
  * @param factoryFunction Function to use to re/compute the returned observable.
- * @param dependencies Optional. A list of dependencies used by the `factoryFunction` function.
+ * @param dependencies A list of dependencies used by the `factoryFunction` function.
  * @returns The value produced by the `factoryFunction`.
  */
 export function useFactoryFunction<TValue>(factoryFunction: () => TValue, dependencies: unknown[]): TValue {
 	if (!Array.isArray(dependencies)) {
-		throw new TypeError(`${dependencies} is an Array. For argument input in useFactoryFunction`);
+		throw new TypeError(`${dependencies} is an Array. For argument dependencies in useFactoryFunction`);
 	}
 
-	const observableRef = useRef<TValue | undefined>(undefined);
+	const valueRef = useRef<TValue | undefined>(undefined);
 	const hasDependenciesChanged = useHasInputChanged(dependencies);
-	if (observableRef.current === undefined || hasDependenciesChanged) {
-		const observable = factoryFunction();
-		observableRef.current = observable;
+	if (valueRef.current === undefined || hasDependenciesChanged) {
+		const value = factoryFunction();
+		valueRef.current = value;
 	}
 
-	return observableRef.current as TValue;
+	return valueRef.current as TValue;
 }

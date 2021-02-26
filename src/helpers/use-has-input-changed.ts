b850839eb15fbb3f9171a2a9de1e1c84aa138b8c
Samuel Carnell <samuel.carnell@outlook.com>
2021-02-26 17:58:04 +0000

    Update variable name

--- src/helpers/use-has-input-changed.ts
+++ src/helpers/use-has-input-changed.ts
@@ -12,19 +12,21 @@ function areInputsDifferent<TInput extends any[]>(input1: TInput, input2: TInput
 }
 
 /**
- * Determines if the any of the values in the `input` have changed between re-renders. This function is concurrent mode safe.
+ * Determines if the any of the values in the `input` have changed between re-renders. This function is concurrent
+ * mode safe.
  * @param input The array of values to check.
  * @returns True if any of the values have been changed between re renders, otherwise false.
  * @internal
  */
 export function useHasInputChanged<TInput extends any[]>(input: TInput): boolean {
 	const inputRef = useRef<TInput>(input);
-	const shouldCompute = areInputsDifferent(inputRef.current, input);
+	const isInputDifferent = areInputsDifferent(inputRef.current, input);
 
-	// Only update the inputRef once the render has been committed, this is required as react may call this hook without committing the changes
+	// Only update the inputRef once the render has been committed, this is required as react may call this hook without
+	// committing the changes
 	useEffect(() => {
 		inputRef.current = input;
 	});
 
-	return shouldCompute;
+	return isInputDifferent;
 }

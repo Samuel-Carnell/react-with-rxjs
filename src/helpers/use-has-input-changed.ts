568ed98018198c3cf886ce3f2a382f3e9dbf6158
Samuel Carnell <samuel.carnell@outlook.com>
2021-03-03 21:12:22 +0000

    Update useHasInputChanged

--- src/helpers/use-has-input-changed.ts
+++ src/helpers/use-has-input-changed.ts
@@ -1,13 +1,32 @@
 import { useEffect, useRef } from 'react';
 
-function areInputsDifferent<TInput extends any[]>(input1: TInput, input2: TInput) {
-	const zippedInputs: Array<[any, any]> = Array.from(
-		Array(Math.max(input1.length, input2.length)),
-		(_, index) => [input1[index], input2[index]]
-	);
+function areInputsDifferent<TInput extends any[]>(
+	prevInputs: TInput,
+	nextInputs: TInput,
+	hookName?: string
+) {
+	if (process.env.NODE_ENV === 'development') {
+		// Don't bother comparing lengths in prod because these arrays should be
+		// passed inline.
+		if (nextInputs.length !== prevInputs.length) {
+			console.error(
+				'The final argument passed to %s changed size between renders. The ' +
+					'order and size of this array must remain constant.\n\n' +
+					'Previous: %s\n' +
+					'Incoming: %s',
+				hookName,
+				`[${prevInputs.join(', ')}]`,
+				`[${nextInputs.join(', ')}]`
+			);
+		}
+	}
 
-	// Using Object.is to stay consistent with how other hooks such as useMemo and useEffect compare dependencies
-	return zippedInputs.some(([value1, value2]) => !Object.is(value1, value2));
+	for (let i = 0; i < prevInputs.length && i < nextInputs.length; i++) {
+		if (!Object.is(prevInputs[i], nextInputs[i])) {
+			return false;
+		}
+	}
+	return true;
 }
 
 /**
@@ -17,9 +36,12 @@ function areInputsDifferent<TInput extends any[]>(input1: TInput, input2: TInput
  * @returns True if any of the values have been changed between re renders, otherwise false.
  * @internal
  */
-export function useHasInputChanged<TInput extends any[]>(input: TInput): boolean {
+export function useHasInputChanged<TInput extends any[]>(
+	input: TInput,
+	hookName?: string
+): boolean {
 	const inputRef = useRef<TInput>(input);
-	const isInputDifferent = areInputsDifferent(inputRef.current, input);
+	const isInputDifferent = areInputsDifferent(inputRef.current, input, hookName);
 
 	// Only update the inputRef once the render has been committed, this is required as react may call this hook without
 	// committing the changes

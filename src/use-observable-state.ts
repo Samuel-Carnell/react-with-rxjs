77d29643fbd369ff55efa42342b9b95c6de55097
Samuel Carnell <samuel.carnell@outlook.com>
2021-03-07 17:14:49 +0000

    wip

--- src/use-observable-state.ts
+++ src/use-observable-state.ts
@@ -4,35 +4,44 @@ import { useHasInputChanged } from './helpers/use-has-input-changed';
 import { useSubscription } from './helpers/use-subscription';
 
 /**
- * Subscribes to the given `observable` and returns the latest value it emitted, the latest error it thrown, and a boolean
- * representing if it has completed. If the given `observable` changes between re-renders then this function subscribes to
+ * Subscribes to `observable` and returns the latest value it emitted, the latest error it thrown, and a boolean
+ * representing if it has completed.
+ *
+ * If `observable` changes between re-renders then this function subscribes to
  * new observable, and disposes of the previous subscription.
- * @typeParam TValue The type of the value emitted by the given `observable`.
- * @typeParam TError The type of error throw by the given `observable`. Defaults to `any`.
- * @param observable The source observable to subscribe to.
+ * @typeParam `TValue` - The type of the value emitted by `observable`.
+ * @typeParam `TError` - The type of error thrown by `observable`. Defaults to `any`.
+ * @param observable - The source observable to subscribe to.
  * @returns
- * * The latest value emitted by the `observable`, or `undefined` if it has not yet emitted a value
- * * The error thrown by the `observable`, or `undefined` if it has not thrown an error.
- * * A boolean representing if the given `observable` is complete. If the `observable` changes between re-renders, then this will be
- * reset to false.
+ *
+ * `[0]` - The latest value emitted by the `observable`, or the `initialValue` if it has not yet emitted a value.
+ *
+ * `[1]` - The error thrown by the `observable`, or `undefined` if it has not thrown an error.
+ *
+ * `[2]` - A boolean representing if the given observable is complete. If the observable changes between re-renders,
+ * then this will be reset to `false`.
  */
 export function useObservableState<TValue, TError = any>(
 	observable: Observable<TValue>
 ): [TValue | undefined, TError | undefined, boolean];
 
 /**
- * Subscribes to the given `observable` and returns the latest value it emitted, the latest error it thrown, and a boolean
- * representing if it has completed. If the given observable changes between re-renders then this function subscribes to
+ * Subscribes to `observable` and returns the latest value it emitted, the latest error it thrown, and a boolean
+ * representing if it has completed.
+ *
+ * If `observable` changes between re-renders then this function subscribes to
  * new observable, and disposes of the previous subscription.
- * @typeParam TValue The type of the value emitted by the given `observable`.
- * @typeParam TError The type of error throw by the given `observable`. Defaults to `any`.
+ * @typeParam `TValue` The type of the value emitted by `observable`.
+ * @typeParam `TError` The type of error thrown by `observable`. Defaults to `any`.
  * @param observable The source observable to subscribe to.
  * @param initialValue The value to return until the given observable has emitted a value.
  * @returns
- * * The latest value emitted by the `observable`, or the initialValue if it has not yet emitted a value
- * * The error thrown by the `observable`, or undefined if it has not thrown an error.
- * * A boolean representing if the given observable is complete. If the observable changes between re-renders, then this will be
- * reset to false.
+ * `[0]` - The latest value emitted by the `observable`, or `initialValue` if it has not yet emitted a value.
+ *
+ * `[1]` - The error thrown by the `observable`, or `undefined` if it has not thrown an error.
+ *
+ * `[2]` - A boolean representing if the given observable is complete. If the observable changes between re-renders,
+ * then this will be reset to `false`.
  */
 export function useObservableState<TValue, TError = any>(
 	observable: Observable<TValue>,

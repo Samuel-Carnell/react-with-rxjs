ea5e48b1e5f2c2370d362a1aab69263ca87326fe
Samuel Carnell <samuel.carnell@outlook.com>
2021-03-23 16:00:21 +0000

    Fix useStateObservable not calculating the current state when nothing is subscribed

--- src/use-state-observable.ts
+++ src/use-state-observable.ts
@@ -1,11 +1,36 @@
 import { useCallback, useLayoutEffect } from 'react';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { distinctUntilChanged, scan } from 'rxjs/operators';
+import { ConnectableObservable, Observable, OperatorFunction, pipe, Subject } from 'rxjs';
+import { distinctUntilChanged, publishReplay, scan, startWith } from 'rxjs/operators';
 import { useFactory } from './helpers/use-factory';
 import { isFunction } from './helpers/is-function';
+import { useSubscription } from './helpers/use-subscription';
 
-type ValueOrFactory<TValue> = (() => TValue) | TValue;
-type ValueOrAccumulator<TValue> = ((prevValue: TValue) => TValue) | TValue;
+type Factory<T> = () => T;
+type Operator<T> = (value: T) => T;
+type SetSate<T> = (state: T | Operator<T>) => void;
+
+function publishCurrentState$<TState>(
+	stateOrOperatorFn$: Observable<TState | Operator<TState>>,
+	initialStateOrFactory: TState | Factory<TState>
+): ConnectableObservable<TState> {
+	const initialState: TState = isFunction(initialStateOrFactory)
+		? initialStateOrFactory()
+		: initialStateOrFactory;
+
+	const publishCurrentValue: OperatorFunction<TState | Operator<TState>, TState> = pipe(
+		startWith(initialState),
+		scan((previousValue: TState, valueOrTransform: TState | Operator<TState>) => {
+			return isFunction(valueOrTransform) ? valueOrTransform(previousValue) : valueOrTransform;
+		}),
+		distinctUntilChanged(Object.is),
+		// Using publishReplay rather than shareReplay so the current state is always calculated when the observable is
+		// connected, even if nothing is subscribed to the observable.
+		publishReplay(1)
+	);
+
+	// The pipe operator doesn't infer the return type as ConnectableObservable, so the return type needs to be casted
+	return stateOrOperatorFn$.pipe(publishCurrentValue) as ConnectableObservable<TState>;
+}
 
 /**
  * Returns a new multi-casted observable and an updater function to feed new values to it.
@@ -27,9 +52,9 @@ type ValueOrAccumulator<TValue> = ((prevValue: TValue) => TValue) | TValue;
  *
  * If the next value is the same as the current value, then the observable will not emit the new value.
  */
-export function useStateObservable<TValue>(): [
-	Observable<TValue | undefined>,
-	(value: ValueOrAccumulator<TValue>) => void
+export function useStateObservable<TState>(): [
+	Observable<TState | undefined>,
+	(value: TState | Factory<TState>) => void
 ];
 
 /**
@@ -39,7 +64,7 @@ export function useStateObservable<TValue>(): [
  * representing the current value rather than the raw value.
  * @typeParam `TValue` The type of the value emitted by the returned observable and the first parameter of the updater
  * function.
- * @param initialValue An initial value for the returned observable to emit.
+ * @param initialState An initial value for the returned observable to emit.
  *
  * If a function is provided then it will be called only on the initial render to calculate the initial value.
  * @returns
@@ -53,45 +78,39 @@ export function useStateObservable<TValue>(): [
  *
  * If the next value is the same as the current value, then the observable will not emit the new value.
  */
-export function useStateObservable<TValue>(
-	initialValue: ValueOrFactory<TValue>
-): [Observable<TValue>, (value: ValueOrAccumulator<TValue>) => void];
+export function useStateObservable<TState>(
+	initialState: TState | Factory<TState>
+): [Observable<TState>, SetSate<TState>];
 
 export function useStateObservable(
-	initialValue?: ValueOrFactory<unknown>
-): [Observable<unknown>, (value: ValueOrAccumulator<unknown>) => void] {
-	const valueOrAcc$ = useFactory(
-		() => {
-			const value = isFunction(initialValue) ? initialValue() : initialValue;
-			return new BehaviorSubject<ValueOrAccumulator<unknown>>(value);
-		},
+	initialState?: unknown | Factory<unknown>
+): [Observable<unknown>, SetSate<unknown>] {
+	const stateOrOperatorFn$: Subject<unknown | Operator<unknown>> = useFactory(
+		() => new Subject<unknown | Operator<unknown>>(),
 		[],
 		'useStateObservable'
 	);
 
 	useLayoutEffect(() => {
-		return () => valueOrAcc$.complete();
-	}, [valueOrAcc$]);
+		return () => stateOrOperatorFn$.complete();
+	}, []);
 
-	const updateValue = useCallback(
-		(state: ValueOrAccumulator<unknown>) => {
-			valueOrAcc$.next(state);
+	const setState: SetSate<unknown> = useCallback(
+		(state: unknown | Operator<unknown>) => {
+			const stateOrOperator = state;
+			stateOrOperatorFn$.next(stateOrOperator);
 		},
-		[valueOrAcc$]
+		[stateOrOperatorFn$]
 	);
 
-	const value$: Observable<unknown> = useFactory(
-		() =>
-			valueOrAcc$.pipe(
-				scan((previousState: unknown, stateOrAcc: ValueOrAccumulator<unknown>) => {
-					const state = isFunction(stateOrAcc) ? stateOrAcc(previousState) : stateOrAcc;
-					return state;
-				}),
-				distinctUntilChanged(Object.is)
-			),
-		[valueOrAcc$],
+	// initialState is not specified as a dependency as it is only used on the first render
+	const currentState$: ConnectableObservable<unknown> = useFactory(
+		() => publishCurrentState$(stateOrOperatorFn$, initialState),
+		[stateOrOperatorFn$],
 		'useStateObservable'
 	);
 
-	return [value$, updateValue];
+	useSubscription(() => currentState$.connect(), [currentState$]);
+
+	return [currentState$, setState];
 }

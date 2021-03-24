44f387b07b8cec7bd7f307063c2bf4bb0c29c80c
Samuel Carnell <samuel.carnell@outlook.com>
2021-03-24 10:31:48 +0000

    Update useValueObservable comment

--- src/use-value-observable.ts
+++ src/use-value-observable.ts
@@ -4,14 +4,14 @@ import { useFactory } from './helpers/use-factory';
 import { useHasInputChanged } from './helpers/use-has-input-changed';
 
 /**
- * Returns a observable which watches `value` and emits `value` on the first render, then when `value` changes between
+ * Returns a observable which watches `value` and emits `value` when the component mounts, then when `value` changes between
  * re-renders.
  *
  * This is useful for converting raw values returned from other hooks such as `useContext`, into observables of those
  * values.
  *
- * @param value The value to watch, and emit on the first render and when changed.
- * @returns An observable which emits `value` on the first render, then when `value` changes between re-renders.
+ * @param value The value to watch, and emit when the component mounts, then when `value` changes.
+ * @returns An observable which emits `value` when the component mounts, then when `value` changes between re-renders.
  *
  * This observable will replay the latest `value` when subscribed to, and complete when the component is unmounted.
  */

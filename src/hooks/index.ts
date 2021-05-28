206b78a75d5a035de4c3067395da24d9ab2c4c93
Samuel Carnell <samuel.carnell@outlook.com>
2021-05-28 10:38:42 +0100

    Resolve merge conflicts

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -3,8 +3,5 @@ export * from './use-observable';
 export * from './use-observable-of';
 export * from './use-state-observable';
 export * from './use-observable-state';
-<<<<<<< HEAD
 export * from './use-subscription';
-=======
 export * from './use-is-complete';
->>>>>>> master
--- src/hooks/internal/index.ts
+++ src/hooks/internal/index.ts
@@ -1,7 +1,3 @@
 export * from './use-factory';
 export * from './use-has-input-changed';
-<<<<<<< HEAD
-=======
-export * from './use-subscription';
 export * from './use-throw-component-error';
->>>>>>> master

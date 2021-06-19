70c54c716fd458e90befde5ff6aa10ce9e7cd222
Samuel Carnell <samuel.carnell@outlook.com>
2021-06-19 18:39:21 +0100

    Remove useObservableState

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,6 +2,5 @@ export * from './use-event-observable';
 export * from './use-observable';
 export * from './use-observable-of';
 export * from './use-state-observable';
-export * from './use-observable-state';
 export * from './use-subscription';
 export * from './use-is-complete';

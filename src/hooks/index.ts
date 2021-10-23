20545d3c99d51958f3bdea7f108c5ad219871fcd
Samuel Carnell <samuel.carnell@outlook.com>
2021-10-23 13:13:38 +0100

    fix: export useLatestValue
    Exports useLatestValue from this library

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -5,3 +5,4 @@ export * from './use-observable-of';
 export * from './use-state-observable';
 export * from './use-subscription';
 export * from './use-is-complete';
+export * from './use-latest-value';
